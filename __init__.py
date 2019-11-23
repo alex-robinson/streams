3c77ca3a46997e2beaca185e7f0103cee607d29a
Alexander Robinson <robinson@fis.ucm.es>
2019-11-24 00:34:02 +0100

    Added package setup files.

---
+++ __init__.py
@@ -0,0 +1,3 @@
+import exergy 
+import streams 
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="streams-rbnsn", # Replace with your own username
+    version="0.0.1",
+    author="Alexander Robinson and Fontina Petrakopoulou",
+    author_email="robinson@ucm.es",
+    description="A package for calculating exergy.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/fontina-petrakopoulou/streams",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
